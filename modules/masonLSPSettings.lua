72bbfb2149a0010bfb6b73b888f6c64fbe6d6b43
warpaint <warpaint9299@email.com>
2024-03-20 01:59:21 +0800

    update

--- modules/masonLSPSettings.lua
+++ modules/masonLSPSettings.lua
@@ -16,7 +16,6 @@ lspconfig.rust_analyzer.setup({
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -33,16 +32,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
 		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
---		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
---		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
---		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
---		vim.keymap.set('n', '<leader>wl', function()
---			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---		end, opts)
---		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
---		vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
---		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 	end,
 })
 
--- settings.lua
+++ settings.lua
@@ -15,10 +15,10 @@ function settings.generic()
 	vim.opt.splitbelow = true
 	vim.opt.ignorecase = false
 	vim.opt.autoindent = true
-	vim.opt.encoding = 'utf-8'
-	vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+	vim.opt.encoding = "utf-8"
+	vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 	--	vim.opt.iskeyword = 'a-z'
-	vim.cmd [[colorscheme tokyonight-moon]]
+	vim.cmd([[colorscheme tokyonight-moon]])
 	vim.cmd([[
 	augroup RememberCursorPosition
 	  autocmd!
@@ -28,47 +28,67 @@ function settings.generic()
 end
 
 function settings.keymap()
-	local norkeymap_options = { noremap = true, silent = true };
-	vim.keymap.set('i', 'jk', '<Esc>', nokeymap_options);
-	vim.keymap.set('i', '<UP>', '<NOP>', nokeymap_options);
-	vim.keymap.set('i', '<DOWN>', '<NOP>', nokeymap_options);
-	vim.keymap.set('i', '<LEFT>', '<NOP>', nokeymap_options);
-	vim.keymap.set('i', '<RIGHT>', '<NOP>', nokeymap_options);
-	vim.keymap.set('i', '<C-b>', '<C-h>', nokeymap_options);
-	vim.keymap.set('i', '<C-h>', '<LEFT>', nokeymap_options);
-	vim.keymap.set('i', '<C-l>', '<RIGHT>', nokeymap_options);
-	vim.keymap.set('i', '<C-j>', '<DOWN>', nokeymap_options);
-	vim.keymap.set('i', '<C-k>', '<UP>', nokeymap_options);
+	local norkeymap_options = { noremap = true, silent = true }
+	vim.keymap.set("i", "jk", "<Esc>", nokeymap_options)
+	vim.keymap.set("i", "<UP>", "<NOP>", nokeymap_options)
+	vim.keymap.set("i", "<DOWN>", "<NOP>", nokeymap_options)
+	vim.keymap.set("i", "<LEFT>", "<NOP>", nokeymap_options)
+	vim.keymap.set("i", "<RIGHT>", "<NOP>", nokeymap_options)
+	vim.keymap.set("i", "<C-b>", "<C-h>", nokeymap_options)
+	vim.keymap.set("i", "<C-h>", "<LEFT>", nokeymap_options)
+	vim.keymap.set("i", "<C-l>", "<RIGHT>", nokeymap_options)
+	vim.keymap.set("i", "<C-j>", "<DOWN>", nokeymap_options)
+	vim.keymap.set("i", "<C-k>", "<UP>", nokeymap_options)
 	-- keys of split window
-	vim.keymap.set('n', 'sp', ':split', {});
-	vim.keymap.set('n', 'vsp', ':vertical split', {});
-	vim.keymap.set('n', '<A-UP>', ':horizontal resize +5<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-DOWN>', ':horizontal resize -5<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-RIGHT>', ':vertical resize -10<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-LEFT>', ':vertical resize +10<CR>', norkeymap_options);
-	vim.keymap.set('n', '<C-UP>', ':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:topleft split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>', norkeymap_options);
-	vim.keymap.set('n', '<C-DOWN>', ':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:botright split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>', norkeymap_options);
-	vim.keymap.set('n', '<C-LEFT>', ':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:vertical topleft split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>', norkrrmap_options);
-	vim.keymap.set('n', '<C-RIGHT>', ':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:vertical botright split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-S-UP>', ':topleft split<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-S-DOWN>', ':botright split<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-S-LEFT>', ':vertical topleft split<CR>', norkeymap_options);
-	vim.keymap.set('n', '<A-S-RIGHT>', ':vertical botright split<CR>', norkeymap_options);
-	vim.keymap.set('n', '<S-c>', ':close<CR>', norkeymap_options);
-	vim.keymap.set('v', '<C-c>', 'y"*', norkeymap_options);
+	vim.keymap.set("n", "sp", ":split", {})
+	vim.keymap.set("n", "vsp", ":vertical split", {})
+	vim.keymap.set("n", "<A-UP>", ":horizontal resize +5<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-DOWN>", ":horizontal resize -5<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-RIGHT>", ":vertical resize -10<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-LEFT>", ":vertical resize +10<CR>", norkeymap_options)
+	vim.keymap.set(
+		"n",
+		"<C-UP>",
+		':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:topleft split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>',
+		norkeymap_options
+	)
+	vim.keymap.set(
+		"n",
+		"<C-DOWN>",
+		':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:botright split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>',
+		norkeymap_options
+	)
+	vim.keymap.set(
+		"n",
+		"<C-LEFT>",
+		':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:vertical topleft split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>',
+		norkrrmap_options
+	)
+	vim.keymap.set(
+		"n",
+		"<C-RIGHT>",
+		':let @r=line(".")<CR>:let @c=col(".")<CR>:let @p=@%<CR>:close<CR>:vertical botright split <C-r>p<CR>:<C-r>r<CR>:normal <C-r>c|<CR>',
+		norkeymap_options
+	)
+	vim.keymap.set("n", "<A-S-UP>", ":topleft split<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-S-DOWN>", ":botright split<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-S-LEFT>", ":vertical topleft split<CR>", norkeymap_options)
+	vim.keymap.set("n", "<A-S-RIGHT>", ":vertical botright split<CR>", norkeymap_options)
+	vim.keymap.set("n", "<S-c>", ":close<CR>", norkeymap_options)
+	vim.keymap.set("v", "<C-c>", 'y"*', norkeymap_options)
 	-- reset default register of keys to avoid overwrite system clipboard
-	vim.keymap.set({ 'n', 'v' }, 'x', '"xx', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'd', '"dd', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 's', '"ss', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'c', '"cc', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'px', '"xp', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'pd', '"dp', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'ps', '"sp', norkeymap_options);
-	vim.keymap.set({ 'n', 'v' }, 'pc', '"cp', norkeymap_options);
-	vim.g.mapleader = ' '
-	vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', norkeymap_options);
-	vim.keymap.set('n', '<leader>ss', ':SymbolsOutline<CR>', norkeymap_options);
-	vim.keymap.set('n', '<leader>f', ':Format<CR>', norkeymap_options);
+	vim.keymap.set({ "n", "v" }, "x", '"xx', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "d", '"dd', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "s", '"ss', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "c", '"cc', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "px", '"xp', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "pd", '"dp', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "ps", '"sp', norkeymap_options)
+	vim.keymap.set({ "n", "v" }, "pc", '"cp', norkeymap_options)
+	vim.g.mapleader = " "
+	vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<CR>", norkeymap_options)
+	vim.keymap.set("n", "<leader>ss", ":SymbolsOutline<CR>", norkeymap_options)
+	vim.keymap.set("n", "<leader>f", ":Format<CR>", norkeymap_options)
 end
 
-return settings;
+return settings
