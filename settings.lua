dae79df3c5804dbf99f73ac265141b77f95b752f
warpaint <warpaint9299@email.com>
2024-03-21 20:45:21 +0800

    update

--- settings.lua
+++ settings.lua
@@ -20,12 +20,47 @@ function settings.generic()
 	--	vim.opt.iskeyword = 'a-z'
 	--	vim.cmd([[colorscheme tokyonight-moon]])
 	vim.cmd([[colorscheme gruvbox]])
-	vim.cmd([[
-	augroup RememberCursorPosition
-	  autocmd!
-	  autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
-	augroup END
-	]])
+	vim.cmd(
+			[[
+				augroup RememberCursorPosition
+				  autocmd!
+				  autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
+				augroup END
+			]]
+	)
+	vim.cmd(
+			[[
+				let g:term_buf = 0
+				let g:term_win = 0
+				function! TermToggle(height)
+					if win_gotoid(g:term_win)
+						hide
+					else
+						botright new
+						exec "resize " . a:height
+						try
+							exec "buffer " . g:term_buf
+						catch
+							call termopen($SHELL, {"detach": 0})
+							let g:term_buf = bufnr("")
+							set nonumber
+							set norelativenumber
+							set signcolumn=no
+						endtry
+						startinsert!
+						let g:term_win = win_getid()
+					endif
+				endfunction
+				" Toggle terminal on/off (neovim)
+				nnoremap <A-t> :call TermToggle(12)<CR>
+				inoremap <A-t> <Esc>:call TermToggle(12)<CR>
+				tnoremap <A-t> <C-\><C-n>:call TermToggle(12)<CR>
+
+				" Terminal go back to normal mode
+				tnoremap <Esc> <C-\><C-n>
+				tnoremap :q! <C-\><C-n>:q!<CR>
+			]]
+	)
 end
 
 function settings.keymap()
@@ -89,6 +124,7 @@ function settings.keymap()
 	vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<CR>", norkeymap_options)
 	vim.keymap.set("n", "<leader>ss", ":SymbolsOutline<CR>", norkeymap_options)
 	vim.keymap.set("n", "<leader>f", ":Format<CR>", norkeymap_options)
+	vim.keymap.set("c", ":Term", ":<CR>", norkeymap_options)
 end
 
 return settings
