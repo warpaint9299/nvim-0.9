26d2438dadfd7cd4c597291075f56595ebb1c775
warpaint <warpaint9299@email.com>
2024-03-24 12:56:30 +0800

    update

--- modules/luaSnipSettings.lua
+++ modules/luaSnipSettings.lua
@@ -1,8 +1,8 @@
 local luasnip = {
-	vim.keymap.set({ "i", "s" }, "<S-l>", function()
+	vim.keymap.set({ "i", "s" }, "<S-n>", function()
 		require("luasnip").jump(1)
 	end, { noremap = true, silent = true }),
-	vim.keymap.set({ "i", "s" }, "<S-h>", function()
+	vim.keymap.set({ "i", "s" }, "<S-p>", function()
 		require("luasnip").jump(-1)
 	end, { noremap = true, silent = true }),
 }
