cbd838017bc35ac7e64ba077010bd65311983916
warpaint <warpaint9299@email.com>
2024-03-26 22:01:03 +0800

    update

--- modules/fzfLuaSettings.lua
+++ modules/fzfLuaSettings.lua
@@ -1,5 +1,6 @@
 local fzf = require("fzf-lua").setup({})
-vim.keymap.set("n", "<S-f>", ":lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set("n", "<S-e>", ":lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set("n", "<S-f>", ":lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
 vim.keymap.set("n", "<S-b>", ":lua require('fzf-lua').buffers()<CR>", { silent = true })
 vim.keymap.set("n", "<S-l>", ":lua require('fzf-lua').lsp_finder()<CR>", { silent = true })
 vim.keymap.set("n", "<S-d>", ":lua require('fzf-lua').lsp_document_diagnostics()<CR>", { silent = true })
