6abcaf624b0369909847d7e1b77e44432bbbd818
warpaint <warpaint9299@email.com>
2024-03-26 23:30:30 +0800

    update

--- modules/fzfLuaSettings.lua
+++ modules/fzfLuaSettings.lua
@@ -1,9 +1,8 @@
 local fzf = require("fzf-lua").setup({})
 vim.keymap.set("n", "<S-e>", ":lua require('fzf-lua').files()<CR>", { silent = true })
 vim.keymap.set("n", "<S-f>", ":lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
+vim.keymap.set("n", "<S-m>", ":lua require('fzf-lua').lsp_finder()<CR>", { silent = true })
 vim.keymap.set("n", "<S-b>", ":lua require('fzf-lua').buffers()<CR>", { silent = true })
-vim.keymap.set("n", "<S-l>", ":lua require('fzf-lua').lsp_finder()<CR>", { silent = true })
 vim.keymap.set("n", "<S-d>", ":lua require('fzf-lua').lsp_document_diagnostics()<CR>", { silent = true })
 vim.keymap.set("n", "<S-k>", ":lua require('fzf-lua').keymaps()<CR>", { silent = true })
-vim.keymap.set("n", "<S-m>", ":lua require('fzf-lua').marks()<CR>", { silent = true })
 return fzf
--- modules/luaSnipSettings.lua
+++ modules/luaSnipSettings.lua
@@ -1,8 +1,8 @@
 local luasnip = {
-	vim.keymap.set({ "i", "s", "n" }, "<C-n>", function()
+	vim.keymap.set({ "i", "s", "n" }, "<S-j>", function()
 		require("luasnip").jump(1)
 	end, { noremap = true, silent = true }),
-	vim.keymap.set({ "i", "s", "n" }, "<C-p>", function()
+	vim.keymap.set({ "i", "s", "n" }, "<S-k>", function()
 		require("luasnip").jump(-1)
 	end, { noremap = true, silent = true }),
 }
