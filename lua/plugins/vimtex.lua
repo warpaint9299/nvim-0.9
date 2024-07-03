aa30a96966ccc9cde226c36c527baa69dc94517b
warpaint <warpaint9299@email.com>
2024-07-03 20:53:33 +0800

    update

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,5 +6,6 @@ return {
 		-- VimTeX configuration goes here, e.g.
 		vim.g.vimtex_view_general_viewer = "okular"
 		vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+		vim.g.vimtex_quickfix_open_on_warning = 0
 	end,
 }
