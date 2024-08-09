3747756590332fbe21678cc808a4f176752a86e0
warpaint <warpaint9299@email.com>
2024-08-09 23:11:53 +0800

    add colorscheme catppuccin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,9 @@
 return {
 	-- disable
-	{ "svrana/neosolarized.nvim", enabled = false },
+	{ "catppuccin/nvim", enabled = true },
 	{ "folke/tokyonight.nvim", enabled = false },
+	{ "marko-cerovac/material.nvim", enabled = false },
 	{ "folke/which-key.nvim", enabled = true },
-	{ "marko-cerovac/material.nvim", enabled = true },
 	{ "ellisonleao/gruvbox.nvim", enabled = false },
 	{ "nvimdev/lspsaga.nvim", enabled = true },
 	{ "ray-x/lsp_signature.nvim", enabled = true },
