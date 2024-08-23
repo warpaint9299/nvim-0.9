c48ea6d844c64b078d30dcf5521170b0e75f5343
warpaint <warpaint9299@email.com>
2024-08-24 00:22:27 +0800

    update keymap and delete unused plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 return {
 	-- disable
 	{ "catppuccin/nvim", enabled = false },
-	{ "marko-cerovac/material.nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "ellisonleao/gruvbox.nvim", enabled = true },
-	{ "gennaro-tedesco/nvim-peekup", enabled = false },
 }
--- lua/plugins/material.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"marko-cerovac/material.nvim",
-	config = function()
-		require("material").setup()
-		vim.cmd([[colorscheme material-deep-ocean]])
-	end,
-}
