f4df361b9f4ef6aa9c1249922bf2c4068b9d1eb9
warpaint <warpaint9299@email.com>
2024-08-23 14:59:14 +0800

    config flit.lua

--- lua/plugins/flit.lua
+++ lua/plugins/flit.lua
@@ -16,5 +16,15 @@ return {
 			-- E.g.: opts = { equivalence_classes = {} }
 			opts = {},
 		})
+		-- "clever-a"
+		vim.keymap.set({ "n", "x", "o" }, "ga", function()
+			local sk = vim.deepcopy(require("leap").opts.special_keys)
+			-- The items in `special_keys` can be both strings or tables - the
+			-- shortest workaround might be the below one:
+			sk.next_target = vim.fn.flatten(vim.list_extend({ "A" }, { sk.next_target }))
+			sk.prev_target = vim.fn.flatten(vim.list_extend({ "a" }, { sk.prev_target }))
+
+			require("leap.treesitter").select({ opts = { special_keys = sk } })
+		end, { desc = "Leap Treesitter selection" })
 	end,
 }
