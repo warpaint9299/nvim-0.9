f69dd840fca1ffc7183f1b56692a4a9bef01fd22
warpaint <warpaint9299@email.com>
2024-08-27 15:31:58 +0800

    changing the keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,16 +11,16 @@ vim.keymap.set("i", "<RIGHT>", "<NOP>", keyopts)
 vim.keymap.set("i", "<C-l>", "<RIGHT>", keyopts)
 
 -- keys of moving window
-vim.keymap.set("n", ";k", ":wincmd K<CR>", keyopts)
-vim.keymap.set("n", ";j", ":wincmd J<CR>", keyopts)
-vim.keymap.set("n", ";l", ":wincmd L<CR>", keyopts)
-vim.keymap.set("n", ";h", ":wincmd H<CR>", keyopts)
+vim.keymap.set("n", "<leader>;k", ":wincmd K<CR>", keyopts)
+vim.keymap.set("n", "<leader>;j", ":wincmd J<CR>", keyopts)
+vim.keymap.set("n", "<leader>;l", ":wincmd L<CR>", keyopts)
+vim.keymap.set("n", "<leader>;h", ":wincmd H<CR>", keyopts)
 
 -- keys of resize window
-vim.keymap.set("n", ";<S-k>", ":horizontal resize +5<CR>", keyopts)
-vim.keymap.set("n", ";<S-j>", ":horizontal resize -5<CR>", keyopts)
-vim.keymap.set("n", ";<S-l>", ":vertical resize -10<CR>", keyopts)
-vim.keymap.set("n", ";<S-h>", ":vertical resize +10<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<S-k>", ":horizontal resize +5<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<S-j>", ":horizontal resize -5<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<S-l>", ":vertical resize -10<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<S-h>", ":vertical resize +10<CR>", keyopts)
 
 -- keys of delete buffer or close tab
 vim.keymap.set("n", "<S-c>", ":close<CR>", keyopts)
@@ -30,19 +30,19 @@ vim.keymap.set("n", "<S-d>", ":bdelete<CR>", keyopts)
 vim.keymap.set("n", "<leader>f", ":Format<CR>", keyopts)
 
 -- lsp
-vim.keymap.set("n", ";1", ":Lspsaga code_action<CR>", keyopts)
+vim.keymap.set("n", "<leader>;1", ":Lspsaga code_action<CR>", keyopts)
 vim.keymap.set("n", "gd", ":Lspsaga goto_definition<CR>", keyopts)
 
 -- clipboard register
 vim.keymap.set("v", "<C-c>", '"+y', keyopts)
 
 -- translate
-vim.keymap.set("v", ";t", "<cmd>TransToZH<CR>", keyopts)
-vim.keymap.set("n", ";t", "<cmd>TransToEN<CR>")
+vim.keymap.set("v", "<leader>;t", "<cmd>TransToZH<CR>", keyopts)
+vim.keymap.set("n", "<leader>;t", "<cmd>TransToEN<CR>")
 
 -- tab and buffer
 vim.keymap.set("n", "<tab>", ":bnext<CR>", keyopts)
 vim.keymap.set("n", "<S-tab>", ":bprevious<CR>", keyopts)
-vim.keymap.set("n", ";te", ":tabedit<CR>", keyopts)
-vim.keymap.set("n", ";<tab>", ":tabnext<CR>", keyopts)
-vim.keymap.set("n", ";<S-tab>", ":tabprevious<CR>", keyopts)
+vim.keymap.set("n", "<leader>;te", ":tabedit<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<tab>", ":tabnext<CR>", keyopts)
+vim.keymap.set("n", "<leader>;<S-tab>", ":tabprevious<CR>", keyopts)
