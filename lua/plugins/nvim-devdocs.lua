6847d179810234c23cfcdfd95ba74c3a109cad13
warpaint <warpaint9299@email.com>
2024-08-27 08:16:13 +0800

    set keymap for nvim-devdocs

--- lua/plugins/nvim-devdocs.lua
+++ lua/plugins/nvim-devdocs.lua
@@ -8,7 +8,7 @@ return {
 	config = function()
 		require("nvim-devdocs").setup({
 			dir_path = vim.fn.stdpath("data") .. "/devdocs", -- installation directory
-			hold_buf = true, -- keeping buffers open after closing.
+			hold_buf = false, -- keeping buffers open after closing.
 			open_mode = "vsplit", -- 'split' (horizontal), 'vert' (vertical), and 'tab' for tab edit
 			ensure_installed = {
 				"bash",
@@ -17,6 +17,7 @@ return {
 				"css",
 				"django-5.1",
 				"docker",
+				"dom",
 				"git",
 				"go",
 				"haproxy-2.9",
@@ -26,7 +27,6 @@ return {
 				"kubectl",
 				"kubernetes",
 				"lua-5.4",
-				"man",
 				"markdown",
 				"moment",
 				"moment_timezone",
@@ -49,6 +49,24 @@ return {
 				"vite",
 				"vue-3",
 			},
+			filetypes = {
+				-- extends the filetype to docs mappings used by the `DevdocsOpenCurrent` command, the version doesn't have to be specified
+				-- scss = "sass",
+				bash = "bash",
+				c = "c",
+				cpp = { "c", "cpp" },
+				css = "css",
+				go = "go",
+				html = { "dom", "html" },
+				java = { "openjdk", "spring_boot" },
+				javascript = { "dom", "http", "javascript", "node", "vite" },
+				javascriptreact = { "dom", "http", "javascript", "node", "react", "react_native", "vite" },
+				lua = "lua",
+				python = { "django", "http", "numpy", "python", "pandas", "requests" },
+				rust = "rust",
+				typescript = { "dom", "http", "node", "typescript", "vite" },
+				typescriptrect = { "dom", "http", "node", "typescript", "react", "react_native", "vite" },
+			},
 			telescope = {}, -- passed to the telescope picker
 			float_win = { -- passed to nvim_open_win(), see :h api-floatwin
 				relative = "editor",
@@ -57,8 +75,8 @@ return {
 				border = "rounded",
 			},
 			wrap = false, -- text wrap, only applies to floating window
-			previewer_cmd = "glow", -- for example: "glow"
-			cmd_args = { "-s", "dark", "-w", "80" }, -- example using glow: { "-s", "dark", "-w", "80" }
+			previewer_cmd = nil, -- for example: "glow"
+			cmd_args = {}, -- example using glow: { "-s", "dark", "-w", "80" }
 			cmd_ignore = {}, -- ignore cmd rendering for the listed docs
 			picker_cmd = false, -- use cmd previewer in picker preview
 			picker_cmd_args = {}, -- example using glow: { "-s", "dark", "-w", "50" }
@@ -66,5 +84,7 @@ return {
 				open_in_browser = "",
 			},
 		})
+		vim.api.nvim_set_keymap("n", "<leader>;", ":DevdocsOpenCurrent<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>,", ":DevdocsOpen ", { noremap = true })
 	end,
 }
