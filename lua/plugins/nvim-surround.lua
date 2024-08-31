864473ec7c2a8bd20c54462c667e751d101b9fd4
warpaint <warpaint9299@email.com>
2024-09-01 00:02:41 +0800

    config lable for Flash plugin

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -80,7 +80,49 @@ return {
 			},
 		},
 		config = function()
-			require("flash").setup({})
+			require("flash").setup({
+				label = {
+					-- allow uppercase labels
+					uppercase = true,
+					-- add any labels with the correct case here, that you want to exclude
+					exclude = "",
+					-- add a label for the first match in the current window.
+					-- you can always jump to the first match with `<CR>`
+					current = true,
+					-- show the label after the match
+					after = false, ---@type boolean|number[]
+					-- show the label before the match
+					before = true, ---@type boolean|number[]
+					-- position of the label extmark
+					style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
+					-- flash tries to re-use labels that were already assigned to a position,
+					-- when typing more characters. By default only lower-case labels are re-used.
+					reuse = "lowercase", ---@type "lowercase" | "all" | "none"
+					-- for the current window, label targets closer to the cursor first
+					distance = true,
+					-- minimum pattern length to show labels
+					-- Ignored for custom labelers.
+					min_pattern_length = 0,
+					-- Enable this to use rainbow colors to highlight labels
+					-- Can be useful for visualizing Treesitter ranges.
+					rainbow = {
+						enabled = false,
+						-- number between 1 and 9
+						shade = 5,
+					},
+					-- With `format`, you can change how the label is rendered.
+					-- Should return a list of `[text, highlight]` tuples.
+					---@class Flash.Format
+					---@field state Flash.State
+					---@field match Flash.Match
+					---@field hl_group string
+					---@field after boolean
+					---@type fun(opts:Flash.Format): string[][]
+					format = function(opts)
+						return { { opts.match.label, opts.hl_group } }
+					end,
+				},
+			})
 		end,
 	},
 }
