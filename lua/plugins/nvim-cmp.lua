dfaea132c8af2b6d99c2193d82653267530a3b6a
warpaint <warpaint9299@email.com>
2025-02-25 18:14:43 +0800

    update nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -115,8 +115,6 @@ return {
 						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 					elseif vim.fn["vsnip#available"](1) == 1 then
 						feedkey("<Plug>(vsnip-expand-or-jump)", "")
-					elseif has_words_before() then
-						cmp.complete()
 					else
 						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
@@ -127,6 +125,8 @@ return {
 						cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
 					elseif vim.fn["vsnip#jumpable"](-1) == 1 then
 						feedkey("<Plug>(vsnip-jump-prev)", "")
+					elseif has_words_before() then
+						cmp.complete()
 					end
 				end, { "i", "s" }),
 			},
