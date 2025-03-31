b988792c82ee96ffb10236b7ed9e18c8b770e66f
warpaint <warpaint9299@email.com>
2025-03-31 19:43:44 +0800

    udpate windows.nvim config

--- lua/plugins/windows.lua
+++ lua/plugins/windows.lua
@@ -1,16 +1,31 @@
 return {
 	{
 		"anuvyklack/windows.nvim",
-		priority = 10000,
+		priority = 1000,
 		dependencies = {
 			"anuvyklack/middleclass",
 			"anuvyklack/animation.nvim",
 		},
 		config = function()
-			vim.o.winwidth = 10
-			vim.o.winminwidth = 10
-			vim.o.equalalways = false
-			require("windows").setup()
+			require("windows").setup({
+				autowidth = {
+					enable = true,
+					winwidth = 5,
+					filetype = {
+						help = 2,
+					},
+				},
+				ignore = {
+					buftype = { "quickfix", "nofile" },
+					filetype = { "snacks_picker_list", "NvimTree", "neo-tree", "undotree", "gundo" },
+				},
+				animation = {
+					enable = true,
+					duration = 300,
+					fps = 30,
+					easing = "in_out_sine",
+				},
+			})
 			local function cmd(command)
 				return table.concat({ "<Cmd>", command, "<CR>" })
 			end
